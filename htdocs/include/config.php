d2b8c49f487b6314bce6f7f801a9a1e41d2216be
Jethro Carr <jethro.carr@jethrocarr.com>
2011-02-24 23:16:41 +0000

    * Tagged version 1.1.0_beta_2
    git-svn-id: svn+ssh://svn.local.jethrocarr.com/home/svn/repositories/oss-ldapauthmanager/trunk@55 34294beb-ade1-45b0-a49e-ffae350aa151

--- htdocs/include/config.php
+++ htdocs/include/config.php
@@ -17,7 +17,7 @@ $GLOBALS["config"] = array();
 
 // define the application details
 $GLOBALS["config"]["app_name"]			= "ldapauthmanager";
-$GLOBALS["config"]["app_version"]		= "1.1.0_beta_1";
+$GLOBALS["config"]["app_version"]		= "1.1.0_beta_2";
 
 // define the schema version required
 $GLOBALS["config"]["schema_version"]		= "20100801";
